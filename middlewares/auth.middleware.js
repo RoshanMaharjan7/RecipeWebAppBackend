da6566726a5a678997c58702536ffbde117c3753
Roshan Maharjan <roshanmaharjan678@gmail.com>
2024-08-08 23:01:14 +0545

    Authenication middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+
+const authenicate = async (req, res, next) =>{
+    const authHeader = req.header('Authorization');
+
+    if(!authHeader){
+        return res.status(401).json({succuss:false, message: "Access denied. No token provided"});
+    }
+
+    const token = authHeader.replace("Bearer ", "");
+    if(!token){
+        return res.status(401).json({succuss:false, message: "Access denied. No token provided"});
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    }catch(error){
+        res.status(400).json({ success: false, message: "Invalid token." });
+    }
+}
+
+module.exports = authenicate;
--- routes/recipes.route.js
+++ routes/recipes.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { getAllRecipes, createRecipe, getRecipeById, updateById, deleteById } = require('../controllers/recipes.controller');
+const authenicate = require('../middlewares/auth.middleware');
 const router = express.Router();
 
-router.get('/', getAllRecipes);
+router.get('/', authenicate, getAllRecipes);
 router.post('/', createRecipe);
 router.get('/:id', getRecipeById);
 router.patch('/:id', updateById);
