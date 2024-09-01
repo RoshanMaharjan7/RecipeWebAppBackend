2e556ba90b0912b1776034d753a58a063582955b
Roshan Maharjan <roshanmaharjan678@gmail.com>
2024-09-01 13:33:26 +0545

    Category search done

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,59 +1,114 @@
-const Category = require('../models/category.model')
+const Category = require("../models/category.model");
 const { uploadOnCloundinary } = require("../utils/cloudinary.js");
 
 // create new Category
 const createCategory = async (req, res) => {
-    const {categoryTitle, categoryImage} = req.body;
+  const { categoryTitle, categoryImage } = req.body;
 
-    try {
-        const imageResult = await uploadOnCloundinary(req.files?.categoryImage[0]?.path) ;
-        const newCategory = new Category({categoryTitle, categoryImage: imageResult.secure_url});
+  try {
+    const imageResult = await uploadOnCloundinary(
+      req.files?.categoryImage[0]?.path
+    );
+    const newCategory = new Category({
+      categoryTitle,
+      categoryImage: imageResult.secure_url,
+    });
 
-        await newCategory.save()
+    await newCategory.save();
 
-        res.status(201).json({ success: true, message: "New Category created successfully", data: newCategory})
-    } catch (error) {
-        res.status(401).json({
-            success: false,
-            message: "Failed to create new category",
-            error: error.message,
-          });
-          console.error("Error", error);
-    }
-}
+    res
+      .status(201)
+      .json({
+        success: true,
+        message: "New Category created successfully",
+        data: newCategory,
+      });
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "Failed to create new category",
+      error: error.message,
+    });
+    console.error("Error", error);
+  }
+};
 
-const getAllCategory = async(req,res) => {
+const getAllCategory = async (req, res) => {
+  try {
+    const categories = await Category.find();
 
-    try {
-        const categories = await Category.find()
-        
-        res.status(201).json({success: true, message: "Successfully fetched all categories", data: categories})
-    } catch (error) {
-        res.status(401).json({
-            success: false,
-            message: "Failed to fetch all categories",
-            error: error.message,
-          });
-          console.error("Error", error);
-    }
-}
+    res
+      .status(201)
+      .json({
+        success: true,
+        message: "Successfully fetched all categories",
+        data: categories,
+      });
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "Failed to fetch all categories",
+      error: error.message,
+    });
+    console.error("Error", error);
+  }
+};
 
 // fetch category by id
-const getCategoryById = async(req,res) => {
-    const {id} = req.params;
+const getCategoryById = async (req, res) => {
+  const { id } = req.params;
 
-    try {
-        const categoryById = await Category.findById(id).populate("recipes")
-        
-        res.status(201).json({success: true, message: "Successfully fetched category by id", data: categoryById})
-    } catch (error) {
-        res.status(401).json({
-            success: false,
-            message: "Failed to find category by id",
-            error: error.message,
-          });
-          console.error("Error", error);
-    }
-}
+  try {
+    const categoryById = await Category.findById(id).populate("recipes");
 
-module.exports = {createCategory, getAllCategory, getCategoryById}
+    res
+      .status(201)
+      .json({
+        success: true,
+        message: "Successfully fetched category by id",
+        data: categoryById,
+      });
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "Failed to find category by id",
+      error: error.message,
+    });
+    console.error("Error", error);
+  }
+};
+
+const searchCategory = async (req, res) => {
+  try {
+    // Get the search query from the request
+    const query = req.body.query.toLowerCase();
+
+    // Fetch all recipes from the database
+    const allCategories = await Category.find();
+
+    // Filter the recipes based on the search query
+    const results = allCategories.filter((recipe) =>
+      recipe.categoryTitle.toLowerCase().includes(query)
+    );
+
+    // Return the matching results
+    res.status(200).json({
+      success: true,
+      data: results,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      success: false,
+      message: "Failed to delete by id",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  createCategory,
+  getAllCategory,
+  getCategoryById,
+  searchCategory,
+};
--- routes/category.route.js
+++ routes/category.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { createCategory, getAllCategory, getCategoryById } = require('../controllers/category.controller');
+const { createCategory, getAllCategory, getCategoryById, searchCategory } = require('../controllers/category.controller');
 const { upload } = require('../middlewares/multer.middleware');
 const router = express.Router();
 
 router.post('/',upload.fields([{ name: "categoryImage", maxCount: 1 }]), createCategory)
 router.get('/',getAllCategory)
 router.get('/:id', getCategoryById)
+router.post('/search', searchCategory)
 
 
 module.exports = router
